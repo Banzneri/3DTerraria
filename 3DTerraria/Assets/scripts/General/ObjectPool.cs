a42de00d54bbbf4c05fa48521530498d2fe31899
HeyyD <heidi.hati@cs.tamk.fi>
2018-07-21 22:48:33 +0300

    Object pool get method

--- 3DTerraria/Assets/scripts/General/ObjectPool.cs
+++ 3DTerraria/Assets/scripts/General/ObjectPool.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : Singleton<ObjectPool> {
+
+    private List<GameObject> gameObjects = new List<GameObject>();
+
     public void Add (GameObject gameObject) {
         gameObject.transform.SetParent(this.transform);
         gameObject.SetActive(false);
+        gameObjects.Add(gameObject);
+    }
+
+    public GameObject GetObject(GameObject gameObject) {
+        int index = gameObjects.IndexOf(gameObject);
+        return gameObjects[index];
     }
 }

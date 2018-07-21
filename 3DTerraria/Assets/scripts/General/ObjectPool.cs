be6c0f33e47d03b8fbada9550fbedfc89123eb6d
HeyyD <heidi.hati@cs.tamk.fi>
2018-07-21 21:16:37 +0300

    Object pool file

---
+++ 3DTerraria/Assets/scripts/General/ObjectPool.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : Singleton<ObjectPool> {
+    // TODO!! If object needs to be instantiated it should be taken from
+    // object-pool, and if object needs to be destroyed it should be moved to
+    // the pool.
+}

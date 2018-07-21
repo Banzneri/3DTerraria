24f4d606c89ec112d480d6b5a51d6a7dc063b75d
HeyyD <heidi.hati@cs.tamk.fi>
2018-07-21 22:24:15 +0300

    Add objects to pool

--- 3DTerraria/Assets/scripts/General/ObjectPool.cs
+++ 3DTerraria/Assets/scripts/General/ObjectPool.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : Singleton<ObjectPool> {
-    // TODO!! If object needs to be instantiated it should be taken from
-    // object-pool, and if object needs to be destroyed it should be moved to
-    // the pool.
+    public void Add (GameObject gameObject) {
+        gameObject.transform.SetParent(this.transform);
+        gameObject.SetActive(false);
+    }
 }

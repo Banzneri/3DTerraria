7ebc2f93c252d140ab918ac0555ba2df03cdcf1d
HeyyD <heidi.hati@cs.tamk.fi>
2018-07-21 21:33:22 +0300

    Delete ObjectPool

--- 3DTerraria/Assets/scripts/General/ObjectPool.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObjectPool : Singleton<ObjectPool> {
-    // TODO!! If object needs to be instantiated it should be taken from
-    // object-pool, and if object needs to be destroyed it should be moved to
-    // the pool.
-}

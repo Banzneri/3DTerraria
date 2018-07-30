87b4b2e2f7ad4681e679a8eccdc0e2df8a4c2b5b
HeyyD <heidi.hati@cs.tamk.fi>
2018-07-30 21:18:32 +0300

    List inventory items according to type

--- 3DTerraria/Assets/scripts/Inventory/InvetoryUI.cs
+++ 3DTerraria/Assets/scripts/Inventory/InvetoryUI.cs
@@ -7,12 +7,11 @@ public class InvetoryUI: MonoBehaviour {
     [SerializeField]
     private Camera UICamera;
     private Camera mainCamera;
-    private Inventory inventory;
+    private Player player;
 
     private void Start() {
         mainCamera = Camera.main;
-        Player p = (Player) FindObjectOfType(typeof(Player));
-        inventory = p.inventory;
+        player = (Player) FindObjectOfType(typeof(Player));
     }
 
     private void Update() {
@@ -24,5 +23,14 @@ public class InvetoryUI: MonoBehaviour {
     private void ToggleInventory() {
         mainCamera.enabled = !mainCamera.enabled;
         UICamera.enabled = !UICamera.enabled;
+
+        if(UICamera.enabled) {
+
+            HashSet<Dictionary<string, int>> set = new HashSet<Dictionary<string, int>>();
+
+            foreach(Item item in player.inventory.Items) {
+                Debug.Log(item.GetType().ToString());
+            }
+        }
     }
 }

378a458fbba92ed733e3482a8c2fc0d68b346d23
HeyyD <heidi.hati@cs.tamk.fi>
2018-07-30 20:48:10 +0300

    Toggle ui

---
+++ 3DTerraria/Assets/scripts/Inventory/InvetoryUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvetoryUI: MonoBehaviour {
+
+    [SerializeField]
+    private Camera UICamera;
+    private Camera mainCamera;
+    private Inventory inventory;
+
+    private void Start() {
+        mainCamera = Camera.main;
+        Player p = (Player) FindObjectOfType(typeof(Player));
+        inventory = p.inventory;
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            ToggleInventory();
+        }
+    }
+
+    private void ToggleInventory() {
+        mainCamera.enabled = !mainCamera.enabled;
+        UICamera.enabled = !UICamera.enabled;
+    }
+}
